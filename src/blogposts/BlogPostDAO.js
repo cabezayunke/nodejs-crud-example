6cc26bb88e2a780d849701e8d23ed9937dfbb919
Jose Ramon Blanco Rey <jose.r.blanco.rey@accenture.com>
2020-01-27 13:56:15 +0100

    simple api with validation , logic and DAL from the router file

--- src/blogposts/BlogPostDAO.js
+++
@@ -1,9 +0,0 @@
-const BlogPostModel = require('./BlogPostModel')
-
-const BlogPostDAO = {
-  getBlogPost: (id) => BlogPostModel.findById(id),
-  createBlogPost: (blogPost) => new BlogPostModel(blogPost),
-  updateBlogPost: (id, blogPost) => BlogPostModel.findOneAndUpdate({ _id: id, }, { ...blogPost }),
-  deleteBlogPost: (id) => BlogPostModel.findById(id).remove(),
-}
-module.exports = BlogPostDAO
--- src/blogposts/BlogPostRouter.js
+++ src/blogposts/BlogPostRouter.js
@@ -1,39 +1,67 @@
+const Joi = require('joi')
 const Koa2JoiValidate = require('koa2-joi-validate')
 const Router = require('koa-router')
 const { useController } = require('../common/ControllerUtils')
-const BlogPostValidation = require('./BlogPostValidation')
-const BlogPostService = require('./BlogPostService')
+const ApiError = require('../common/ApiError')
+const BlogPostModel = require('./BlogPostModel')
 
 const validator = Koa2JoiValidate({ passError: true })
+const mandatoryBlogPostId = Joi.object({
+  id: Joi.string().required(),
+})
+const mandatoryBlogPostFields = Joi.object({
+  title: Joi.string().required(),
+  body: Joi.string().required(),
+})
+
 const router = new Router({ prefix: '/api/v1/blogposts' })
 
 // create routes
-const getBlogPost = ({ data: { id }}) => BlogPostService.getBlogPost(id)
+const getBlogPost = async ({ data: { id }}) => {
+  const blogPost = await BlogPostModel.findById(id)
+  if(!blogPost) {
+    throw ApiError.notFound('Blog post not found')
+  }
+  return blogPost
+}
 router.get(
   '/:id',
-  validator.params(BlogPostValidation.mandatoryBlogPostId),
+  validator.params(mandatoryBlogPostId),
   (context) => useController(context,  getBlogPost)
 )
-const createBlogPost = ({ data }) => BlogPostService.createBlogPost(data)
+const createBlogPost = async ({ data }) => {
+  const blogPost = new BlogPostModel(data)
+  await blogPost.save()
+  return blogPost
+}
 router.post(
   '/',
-  validator.body(BlogPostValidation.mandatoryBlogPostFields),
+  validator.body(mandatoryBlogPostFields),
   (context) => useController(context, createBlogPost)
 )
-const updateBlogPost = ({ data }) => BlogPostService.updateBlogPost(data)
+const updateBlogPost = async ({ data }) => {
+  const blogPost = await BlogPostModel.findOneAndUpdate({ _id: data.id, }, {
+    title: data.title,
+    body: data.body,
+  })
+  if(!blogPost) {
+    throw ApiError.internal('Blog post could not be updated')
+  }
+  return blogPost
+}
 router.put(
   '/:id',
-  validator.params(BlogPostValidation.mandatoryBlogPostId),
-  validator.body(BlogPostValidation.mandatoryBlogPostFields),
+  validator.params(mandatoryBlogPostId),
+  validator.body(mandatoryBlogPostFields),
   (context) => useController(context, updateBlogPost)
 )
 const deleteBlogPost = async ({ data: { id }}) => {
-  await BlogPostService.deleteBlogPost(id)
+  await BlogPostModel.findById(id).remove()
   return { status: 204 }
 }
 router.delete(
   '/:id',
-  validator.params(BlogPostValidation.mandatoryBlogPostId),
+  validator.params(mandatoryBlogPostId),
   (context) => useController(context, deleteBlogPost)
 )
 
